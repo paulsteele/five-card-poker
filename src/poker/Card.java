f87321d48d88b77f7ae1c1fa44c872237cf0697a
paulsteele <paul_steele@live.com>
2013-10-03 14:41:31 -0400

    folder structure changed

---
+++ src/poker/Card.java
@@ -0,0 +1,93 @@
+package poker;
+/**
+ * Card Class
+ * 
+ * Class contains a card value and suit value
+ * Also contains a terminal display method
+ * 
+ * @author Paul Steele
+ *
+ */
+public class Card {
+	private int number; //the value of the card
+	private int suit; //the suit of the card
+	
+	
+	/**
+	 * Card Constructor
+	 * 
+	 * takes in a card value and suit value and stores them in this card
+	 * @param number
+	 * @param suit
+	 */
+	public Card(int number, int suit) {
+		this.number = number;
+		this.suit = suit;
+	}
+	
+	/**
+	 * getNumber()
+	 * 
+	 * returns the value number, not the human readable value
+	 * @return int
+	 */
+	public int getNumber(){
+		return number;
+	}
+	
+	/**
+	 * getSuit()
+	 * 
+	 * returns the suit value, not the human readable value
+	 * @return int
+	 */
+	public int getSuit(){
+		return suit;
+	}
+	
+	/**
+	 * getValue()
+	 * 
+	 * returns the human readable string of the card value e.g. "King", "One"
+	 * @return String
+	 */
+	public String getValue(){
+		switch (number){
+		
+			case 0: return "Ace";
+			case 1: return "Two";
+			case 2: return "Three";
+			case 3: return "Four";
+			case 4: return "Five";
+			case 5: return "Six";
+			case 6: return "Seven";
+			case 7: return "Eight";
+			case 8: return "Nine";
+			case 9: return "Ten";
+			case 10: return "Jack";
+			case 11: return "Queen";
+			case 12: return "King";
+		
+			default: return "NA";
+		}
+		
+	}
+	
+	/**
+	 * getSuitChar()
+	 * 
+	 * returns a char value of the suit e.g. a spade would return ♠ 
+	 * ♠ ♣ ♦ ♥
+	 * @return char
+	 */
+	public char getSuitChar(){
+		switch(suit){
+			case 0: return '♥';
+			case 1: return '♦';
+			case 2: return '♣';
+			case 3: return '♠';
+			
+			default: return 'N';
+		}
+	}
+}
---
+++ src/poker/Deck.java
@@ -0,0 +1,56 @@
+package poker;
+/**
+ * Deck 
+ * 
+ * Class is a collection of 52 cards, all of which make up the playable deck
+ * Has shuffle methods and draw methods and renew
+ * 
+ * @author Paul Steele
+ *
+ */
+
+import java.util.Random; //used for shuffling
+
+
+
+public class Deck{
+	private Card[] deck; //the deck of cards
+	private int remainingCards; //number of remaining cards
+	
+	/**
+	 * Deck Constructor
+	 */
+	public Deck(){
+		deck = new Card[52];//declares deck
+		remainingCards = deck.length; //initializes the remaingCards value
+		int suit; //the suit value
+		int number; //the card number value
+		
+		for(int i = 0; i < remainingCards; i++){ //loops through all cards and assigns a suit and number
+			suit = i / 13; //will ultimately hit 0, 1, 2, and 3 each 14 times
+			number = i % 13; //fills each card number with one value per set
+			deck[i] = new Card(number, suit); //does the actual assigning of cards
+			
+		}
+	}
+	
+	/**
+	 * shuffle()
+	 * 
+	 * Shuffles the deck
+	 */
+	public void shuffle(){
+		Random random = new Random();
+		int firstNumber;
+		int secondNumber;
+		for (int i = 0; i < remainingCards; i++){
+			firstNumber = random.nextInt(remainingCards);
+			secondNumber = random.nextInt(remainingCards);
+			Card temp = deck[firstNumber];
+			deck[firstNumber] = deck[secondNumber];
+			deck[secondNumber] = temp;
+		}
+		
+		
+	}
+}
